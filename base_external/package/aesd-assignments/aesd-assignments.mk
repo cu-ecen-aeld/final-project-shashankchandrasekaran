6049972833b64102e9248f5b64ba4199b76d5ace
Shashank Chandrasekaran <shashuchan@gmail.com>
2023-04-15 21:35:41 +0000

    Testing

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -10,6 +10,7 @@ AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
+# Added to check commit changes 
 AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
